dca5f3f00228c72eccaf86cac39b237e1cfac178
stampylongheadgaming1 <mitchellwears@gmail.com>
2016-07-12 21:39:00 -0400

    Create service.js

---
+++ service.js
@@ -0,0 +1,10 @@
+apply plugin: 'com.android.application'
+    ...
+
+    dependencies {
+        compile 'com.google.android.gms:play-services:9.2.1'
+    }
+com.google.android.gms:play-services:9.2.1
+com.google.android.gms:play-services-fitness:9.2.1'
+compile 'com.google.android.gms:play-services-wearable:9.2.1'
+
